eeea7e06b178556845a3788287fba59788ebb850
fliblip <wickynjy@gmail.com>
2022-09-07 10:38:05 +0300

    added responsive-css

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,11 @@
-const apiHost = 
+fetch('http://localhost:3000/books')
+    .then(function (response) {
+        return response.json();
+})
+
+.then(function (data){
+    console.log(`${data["books"]}`)
+})
 
 document.addEventListener('DOMContentLoaded', () =>{
 
