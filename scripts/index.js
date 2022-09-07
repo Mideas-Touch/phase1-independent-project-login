040ecd8cc4e2e826b4402d25421ede9004c4bb6d
fliblip <wickynjy@gmail.com>
2022-09-07 21:56:06 +0300

    populate bd.json

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
-fetch('http://localhost:3000/books')
-    .then(function (response) {
-        return response.json();
-})
+// fetch('http://localhost:3000/books')
+//     .then(function (response) {
+//         return response.json();
+// })
 
-.then(function (data){
-    console.log(`${data["books"]}`)
-})
+// .then(function (data){
+//     console.log(`${data["books"]}`)
+// })
 
-document.addEventListener('DOMContentLoaded', () =>{
+// document.addEventListener('DOMContentLoaded', () =>{
 
-})
+// })
